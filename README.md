0767c9421e5f0ca0c465d8632d03a0d9cd01ecd6
spechkur <spechkur@mail.ru>
2024-09-15 17:36:20 +0300

    update brain-games.js & cli.js with eslint

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/SofiaPechkur/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/SofiaPechkur/frontend-project-44/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/7b9075bf6360ada41892/maintainability)](https://codeclimate.com/github/SofiaPechkur/frontend-project-44/maintainability)
--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,10 @@
 import readlineSync from 'readline-sync';
 
-export const sayGreeting = () => {
-	const userWelcome = 'Welcome to the Brain Games!';
-	console.log(userWelcome);
-	const userName = readlineSync.question('May I have your name? ');
-	const result = (`Hello, ${userName}!`);
-	console.log(result);
-}
+const sayGreeting = () => {
+  const userWelcome = 'Welcome to the Brain Games!';
+  console.log(userWelcome);
+  const userName = readlineSync.question('May I have your name? ');
+  const result = (`Hello, ${userName}!`);
+  console.log(result);
+};
+export default sayGreeting;
