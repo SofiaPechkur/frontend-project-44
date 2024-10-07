66f1fec2bcc578b3adac6b1f1006370a38454fc8
Sofiya <spechkur@mail.ru>
2024-10-07 16:24:25 +0300

    update src/games/brain-progression.js

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,29 +1,24 @@
 import { getRandomNum, launchGame } from '../index.js';
 
-const getProgression = (step, hiddenIn) => {
+const getProgression = (step) => {
   const lengthProgression = 10;
   const progression = [];
   let nextNumber = 0;
   for (let i = 0; i <= lengthProgression - 1; i += 1) {
     nextNumber += step;
-    if (i === hiddenIn) {
-      progression.push('..');
-    } else {
-      progression.push(nextNumber);
-    }
+    progression.push(nextNumber);
   }
   return progression;
 };
-
-const getCorrectAnswer = (progression, step, hiddenIn) => (progression[hiddenIn + 1] - step);
 
 export default () => {
   const rule = 'What number is missing in the progression?';
   const getDataForLaunchGame = () => {
     const step = getRandomNum();
     const hiddenIn = getRandomNum(1, 8);
-    const progression = getProgression(step, hiddenIn);
-    const correctAnswer = getCorrectAnswer(progression, step, hiddenIn);
+    const progression = getProgression(step);
+    const correctAnswer = progression[hiddenIn];
+    progression[hiddenIn] = '..';
     const question = (`Question: ${progression.join(' ')}`);
     return [question, String(correctAnswer)];
   };
